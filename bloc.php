e3c680eb29328d6f114418750992ee3af6a284cb
jorditosa <jordisato88@gmail.com>
2023-05-14 19:06:36 +0200

    bloc dinamico

--- bloc.php
+++ bloc.php
@@ -14,212 +14,82 @@ includeTemplate('header');
 <main class="container">
     <h1 class="py-4">Noticies d'actualitat mediambiental</h1>
 
+    <div class="filter mb-4 p-4 border rounded bg-dark text-light">
+      <div class="row">
+        <div class="filter-date col">
+          <h5>Ordenar per data:</h5>
+          <div class="input">
+            <label for="dateNew">Més noves</label>
+            <input type="radio" name="filter-date" id="dateNew" value="" checked>
+          </div>
+          <div class="input">
+            <label for="dateOld">Més antigues</label>
+            <input type="radio" name="filter-date" id="dateOld" value="">
+          </div>
+        </div>
+  
+        <div class="filter-title col">
+          <h5>Ordenar per títol:</h5>
+          <div class="input">
+            <label for="dateNew">A - Z</label>
+            <input type="radio" name="filter-title" id="dateNew" value="" checked>
+          </div>
+          <div class="input">
+            <label for="dateOld">Z - A</label>
+            <input type="radio" name="filter-title" id="dateOld" value="">
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="row mb-2">
 
-    <!-- News 1 -->
-    <div class="col-12">
-      <?php
+    <?php
       // Importar datos Json
-      $jsonData = file_get_contents('./api/noticies/post_1.json');
-      $data = json_decode($jsonData);
-      ?>
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-            <?php
-            echo $data->title->ca;
-            ?>
-          <div class="mb-1 text-body-secondary">
-          <?php
-            $dt = $data->date;
-            $dateFormatted = date("d/m/Y", $dt); 
-            echo $dateFormatted; 
-            ?>
-          </div>
-          <div class="row">
-            <div class="card-text mb-auto col-9">
-            <?php
-            $description = $data->description->ca;
-
-            // Si la longitud del contenido es mayor a 120 palabras, corta el contenido y añade puntos suspensivos
-            if (str_word_count($description) > 120) {
-              $words = str_word_count($description, 2);
-              $pos = array_keys($words);
-              $description = substr($description, 0, $pos[120]) . '...';
-            }
-
-            echo $description;
-            echo $data->read_more->ca;
-            ?>
+      $news=[];
+      for($i = 1; $i <= 5; $i++) {
+        $jsonData = file_get_contents('./api/noticies/post_'.$i.'.json');
+        $data = json_decode($jsonData);
+        $news[$i] = $data;
+      }
+  
+    ?>
+    
+    <?php for ($i = 1; $i <= 5; $i++) : ?>
+      <?php $data = $news[$i]; ?>
+      <div class="col-12">
+        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <?php echo $data->title->ca; ?>
+            <div class="mb-1 text-body-secondary">
+              <?php
+              $dt = $data->date;
+              $dateFormatted = date("d/m/Y", $dt);
+              echo $dateFormatted;
+              ?>
             </div>
-            <img class="news-img pt-2 col-3" src="<?php echo $data->image; ?>" alt="imatge noticia">
+            <div class="row">
+              <div class="card-text mb-auto col-9">
+                <?php
+                $description = $data->description->ca;
+
+                // Si la longitud del contenido es mayor a 120 palabras, corta el contenido y añade puntos suspensivos
+                if (str_word_count($description) > 120) {
+                  $words = str_word_count($description, 2);
+                  $pos = array_keys($words);
+                  $description = substr($description, 0, $pos[120]) . '...';
+                }
+
+                echo $description;
+                echo $data->read_more->ca;
+                ?>
+              </div>
+              <img class="news-img pt-2 col-3" src="<?php echo $data->image; ?>" alt="imatge noticia">
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-     <!-- News 2 -->
-     <div class="col-12">
-      <?php
-      // Importar datos Json
-      $jsonData = file_get_contents('./api/noticies/post_2.json');
-      $data = json_decode($jsonData);
-      ?>
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-            <?php
-            echo $data->title->ca;
-            ?>
-          <div class="mb-1 text-body-secondary">
-          <?php
-            $dt = $data->date;
-            $dateFormatted = date("d/m/Y", $dt); 
-            echo $dateFormatted; 
-            ?>
-          </div>
-          <div class="row">
-            <div class="card-text mb-auto col-9">
-            <?php
-            $description = $data->description->ca;
-
-            // Si la longitud del contenido es mayor a 120 palabras, corta el contenido y añade puntos suspensivos
-            if (str_word_count($description) > 120) {
-              $words = str_word_count($description, 2);
-              $pos = array_keys($words);
-              $description = substr($description, 0, $pos[120]) . '...';
-            }
-
-            echo $description;
-            echo $data->read_more->ca;
-            ?>
-            </div>
-            <img class="news-img pt-2 col-3" src="<?php echo $data->image; ?>" alt="imatge noticia">
-          </div>
-        </div>
-      </div>
-    </div>
-
-     <!-- News 3 -->
-     <div class="col-12">
-      <?php
-      // Importar datos Json
-      $jsonData = file_get_contents('./api/noticies/post_3.json');
-      $data = json_decode($jsonData);
-      ?>
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-            <?php
-            echo $data->title->ca;
-            ?>
-          <div class="mb-1 text-body-secondary">
-          <?php
-            $dt = $data->date;
-            $dateFormatted = date("d/m/Y", $dt); 
-            echo $dateFormatted; 
-            ?>
-          </div>
-          <div class="row">
-            <div class="card-text mb-auto col-9">
-            <?php
-            $description = $data->description->ca;
-
-            // Si la longitud del contenido es mayor a 120 palabras, corta el contenido y añade puntos suspensivos
-            if (str_word_count($description) > 120) {
-              $words = str_word_count($description, 2);
-              $pos = array_keys($words);
-              $description = substr($description, 0, $pos[120]) . '...';
-            }
-
-            echo $description;
-            echo $data->read_more->ca;
-            ?>
-            </div>
-            <img class="news-img pt-2 col-3" src="<?php echo $data->image; ?>" alt="imatge noticia">
-          </div>
-        </div>
-      </div>
-    </div>
-
-     <!-- News 4 -->
-     <div class="col-12">
-      <?php
-      // Importar datos Json
-      $jsonData = file_get_contents('./api/noticies/post_4.json');
-      $data = json_decode($jsonData);
-      ?>
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-            <?php
-            echo $data->title->ca;
-            ?>
-          <div class="mb-1 text-body-secondary">
-          <?php
-            $dt = $data->date;
-            $dateFormatted = date("d/m/Y", $dt); 
-            echo $dateFormatted; 
-            ?>
-          </div>
-          <div class="row">
-            <div class="card-text mb-auto col-9">
-            <?php
-            $description = $data->description->ca;
-
-            // Si la longitud del contenido es mayor a 120 palabras, corta el contenido y añade puntos suspensivos
-            if (str_word_count($description) > 120) {
-              $words = str_word_count($description, 2);
-              $pos = array_keys($words);
-              $description = substr($description, 0, $pos[120]) . '...';
-            }
-
-            echo $description;
-            echo $data->read_more->ca;
-            ?>
-            </div>
-            <img class="news-img pt-2 col-3" src="<?php echo $data->image; ?>" alt="imatge noticia">
-          </div>
-        </div>
-      </div>
-    </div>
-
-     <!-- News 5 -->
-     <div class="col-12">
-      <?php
-      // Importar datos Json
-      $jsonData = file_get_contents('./api/noticies/post_5.json');
-      $data = json_decode($jsonData);
-      ?>
-      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-        <div class="col p-4 d-flex flex-column position-static">
-            <?php
-            echo $data->title->ca;
-            ?>
-          <div class="mb-1 text-body-secondary">
-          <?php
-            $dt = $data->date;
-            $dateFormatted = date("d/m/Y", $dt); 
-            echo $dateFormatted; 
-            ?>
-          </div>
-          <div class="row">
-            <div class="card-text mb-auto col-9">
-            <?php
-            $description = $data->description->ca;
-
-            // Si la longitud del contenido es mayor a 120 palabras, corta el contenido y añade puntos suspensivos
-            if (str_word_count($description) > 120) {
-              $words = str_word_count($description, 2);
-              $pos = array_keys($words);
-              $description = substr($description, 0, $pos[120]) . '...';
-            }
-
-            echo $description;
-            echo $data->read_more->ca;
-            ?>
-            </div>
-            <img class="news-img pt-2 col-3" src="<?php echo $data->image; ?>" alt="imatge noticia">
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php endfor; ?>
 
   </div>
 </main>
@@ -228,10 +98,8 @@ includeTemplate('header');
 
 <?php
 includeTemplate('footer');
-?>
-
-<?php
 includeTemplate('scripts');
 ?>
+
 </body>
 </html>
--- includes/functions.php
+++ includes/functions.php
@@ -6,4 +6,17 @@ function includeTemplate( $name ) {
     include TEMPLATES_URL . $name . ".php";
 }
 
+// Funciones para los filtros
+function compararPorFecha($a, $b) {
+    $fechaA = strtotime($a->getAttribute('data-date'));
+    $fechaB = strtotime($b->getAttribute('data-date'));
+    return $fechaB - $fechaA;
+}
+
+function compararPorTitulo($a, $b) {
+    $tituloA = $a->getAttribute('data-title');
+    $tituloB = $b->getAttribute('data-title');
+    return strcmp($tituloA, $tituloB);
+}
+
 ?>
