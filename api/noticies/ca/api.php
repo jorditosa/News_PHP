64a0527439a06adb3f2850a76832b7984a9d7b7e
jorditosa <jordisato88@gmail.com>
2023-05-18 01:00:33 +0200

    api done y composer

--- api/noticies/ca/api.php
+++ api/noticies/ca/api.php
@@ -1,4 +1,6 @@
 <?php
+// Idioma predeterminado
+$idioma = 'ca';
 
 // Establecer las cabeceras para indicar que la respuesta es en formato JSON
 header('Content-Type: application/json');
@@ -6,7 +8,14 @@ header('Content-Type: application/json');
 $news=array();
 for($i = 1, $j = 0; $j < 5; $i++, $j++) {
     $jsonData = file_get_contents('../post_'.$i.'.json');
-    $news[$j] = json_decode($jsonData);
+    $data[$j] = json_decode($jsonData);
+
+    // Montar el array con los valores del idioma seleccionado
+    $news[$j] = array(
+        'title' => $data[$j]->title->$idioma,
+        'content' => $data[$j]->description->$idioma,
+        'cta' => $data[$j]->read_more->$idioma,
+    );    
 }
 
 // Convertir el arreglo en formato JSON
